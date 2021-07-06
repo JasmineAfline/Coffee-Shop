0e74a33e3cc4426bc281a7f4456a024c194c6a20
Sharonoito <oitosharonakoth@gmail.com>
2021-07-06 21:48:39 +0300

    order in images

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-function setNewImage()
-{
+// function setNewImage()
+// {
 //  document.getElementById("coff1"). src="coffee images/Cappucino.jpg";
 //  document.getElementById("coff1").innerHTML="Ingredients"
 
 // var coffee=document.createElement("p")
 // coffee.innerHTML="jskjdalksjdlakmlkajdlakdlkasd"
 // document.getElementById("coff1").appendChild(coffee)
-var image= document.getElementById("coff1")
-var coffee=document.createElement("p")
-coffee.innerHTML="Ingredients"
-image.parentNode.replaceChild(coffee,image)
+// var image= document.getElementById("coff1")
+// var coffee=document.createElement("p")
+// coffee.innerHTML="Ingredients"
+// image.parentNode.replaceChild(coffee,image)
 
-}
-function setOldImage()
-{
+// }
+// function setOldImage()
+// {
     
-    document.getElementById("coff1").src="coffee images/Hazelnut.jpg"
+//     document.getElementById("coff1").src="coffee images/Hazelnut.jpg"
 
-}
+// }
+// // 
+
