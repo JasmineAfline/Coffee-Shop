bcf14f7f80f1555d217983097f6375fee936bfa3
Euniceatieno <eunniceatieno@gmail.com>
2021-07-08 01:30:49 +0300

    payment

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', loaded)
+} else {
+    loaded()
+}
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+}
+function testJS()
+{
+
+var b = document.getElementById('card-text').value
+
+document.getElementById('cart-price').innerHTML = b;
+
+}
+function allItems(){
+    var x=document.getElementById('popupForm')
+    var b=[]
+    b.append(b)
+    document.getElementById('quantity').innerHTML=b.length()
+}
+// function closePay() {
+//     document.getElementById("containers").style.display = "none";
+// }
+// function loaded() {
+//     var addToCart = document.getElementsByClassName('shop')
+//     for (var i = 0; i < addToCart.length; i++) {
+//         var button = addToCart[i]
+//         button.addEventListener('click', buttonClicked)
+//     }
+//     document.getElementsByClassName('shop')[0].addEventListener('click', purchased)
+// }
+
+// function buttonClicked() {
+//     var jina = document.getElementsByClassName('name')[0].value
+//     var pesa = document.getElementsByClassName('item-price')[0].value
+//     var picha = document.getElementsByClassName('item')[0].src
+//     addItemToCart(jina, pesa, picha)
+// }
+// function addItemToCart(name1, cash, picture) {
+//     document.getElementsByClassName('item-name')[0].appendChild(name1)
+//     document.getElementsByClassName('cart-price')[0].appendChild(cash)
+//     document.getElementsByClassName('item-image')[0].appendChild(picture)
+// }
+
+
+
+
+
+
 
 function setNewImage(){
 document.getElementById("coff1").src="tonic.jpg";
