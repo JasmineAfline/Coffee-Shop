75e5a27431b6d38a556317b2bce0f592ae139e67
Euniceatieno <eunniceatieno@gmail.com>
2021-07-09 07:20:03 +0300

    final

--- index.js
+++ index.js
@@ -23,31 +23,18 @@ function allItems(){
     b.append(b)
     document.getElementById('quantity').innerHTML=b.length()
 }
-// function closePay() {
-//     document.getElementById("containers").style.display = "none";
-// }
-// function loaded() {
-//     var addToCart = document.getElementsByClassName('shop')
-//     for (var i = 0; i < addToCart.length; i++) {
-//         var button = addToCart[i]
-//         button.addEventListener('click', buttonClicked)
-//     }
-//     document.getElementsByClassName('shop')[0].addEventListener('click', purchased)
-// }
+function closePay() {
+    alert("Thank you for shopping with us")
+    return (true)
+}
 
-// function buttonClicked() {
-//     var jina = document.getElementsByClassName('name')[0].value
-//     var pesa = document.getElementsByClassName('item-price')[0].value
-//     var picha = document.getElementsByClassName('item')[0].src
-//     addItemToCart(jina, pesa, picha)
-// }
-// function addItemToCart(name1, cash, picture) {
-//     document.getElementsByClassName('item-name')[0].appendChild(name1)
-//     document.getElementsByClassName('cart-price')[0].appendChild(cash)
-//     document.getElementsByClassName('item-image')[0].appendChild(picture)
-// }
+function topFunction(){
+    document.documentElement.scrollTop=0;
+}
 
-
+function openW(){
+    open("#c2",'billingform')
+}
 
 
 
